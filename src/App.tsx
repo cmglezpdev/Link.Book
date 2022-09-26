b24cfd66a5a9f3dd1f7225132f9be1b513933525
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-26 00:13:23 -0400

    chore(Root): add router and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import {
+  goBack,
+  goTo,
+  popToTop,
+  Link,
+  Router,
+  getCurrent,
+  getComponentStack,
+} from 'react-chrome-extension-router';
 
-function App() {
+const Three = ({ message }: any) => (
+  <div onClick={() => popToTop()}>
+    <h1>{message}</h1>
+    <p>Click me to pop to the top</p>
+  </div>
+);
+
+const Two = ({ message }: any) => (
+  <div>
+    This is component Two. I was passed a message:
+    <p>{message}</p>
+    <button onClick={() => goBack()}>
+      Click me to go back to component One
+    </button>
+    <button onClick={() => goTo(Three, { message })}>
+      Click me to go to component Three!
+    </button>
+  </div>
+);
+
+const One = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Link component={Two} props={{ message: 'I came from component one!' }}>
+      This is component One. Click me to route to component Two
+    </Link>
   );
-}
+};
+
+const App = () => {
+  useEffect(() => {
+      const { component, props } = getCurrent();
+      console.log(
+        component
+          ? `There is a component on the stack! ${component} with ${props}`
+          : `The current stack is empty so Router's direct children will be rendered`
+      );
+      const components = getComponentStack();
+      console.log(`The stack has ${components.length} components on the stack`);
+  });
+  return (
+    <Router>
+      <One />
+    </Router>
+  );
+};
 
 export default App;
