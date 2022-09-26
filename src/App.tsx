80fc864215954a97822b722c423960c38085046a
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-26 02:12:16 -0400

    feat(Root): add the structure of principal components

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,16 @@
-import { useEffect } from 'react';
-import {
-  goBack,
-  goTo,
-  popToTop,
-  Link,
-  Router,
-  getCurrent,
-  getComponentStack,
-} from 'react-chrome-extension-router';
-
-const Three = ({ message }: any) => (
-  <div onClick={() => popToTop()}>
-    <h1>{message}</h1>
-    <p>Click me to pop to the top</p>
-  </div>
-);
-
-const Two = ({ message }: any) => (
-  <div>
-    This is component Two. I was passed a message:
-    <p>{message}</p>
-    <button onClick={() => goBack()}>
-      Click me to go back to component One
-    </button>
-    <button onClick={() => goTo(Three, { message })}>
-      Click me to go to component Three!
-    </button>
-  </div>
-);
-
-const One = () => {
-  return (
-    <Link component={Two} props={{ message: 'I came from component one!' }}>
-      This is component One. Click me to route to component Two
-    </Link>
-  );
-};
+import { Menu, Footer } from "./components";
+import { EmptyState } from "./layouts";
 
 const App = () => {
-  useEffect(() => {
-      const { component, props } = getCurrent();
-      console.log(
-        component
-          ? `There is a component on the stack! ${component} with ${props}`
-          : `The current stack is empty so Router's direct children will be rendered`
-      );
-      const components = getComponentStack();
-      console.log(`The stack has ${components.length} components on the stack`);
-  });
   return (
-    <Router>
-      <One />
-    </Router>
-  );
-};
+    <>
+    <main className="bg-slate-200">
+      <Menu />
+      <EmptyState />
+      <Footer />
+    </main>
+    </>
+  )
+}
 
 export default App;
