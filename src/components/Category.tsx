20228a5f429afc10afe9366d3909e9a414c3dea6
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-09-26 02:07:58 -0400

    feat(Root): add category component

---
+++ src/components/Category.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Category = () => {
+    return (
+        <div className='bg-blue-400 w-28 h-28 rounded-lg grid place-content-center cursor-pointer'>
+            <span className='text-xl text-white'>Category</span>
+        </div>
+    )
+}
